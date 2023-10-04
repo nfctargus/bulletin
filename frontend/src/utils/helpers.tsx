e2544e33b12acdd737767324ac5a087e7c456061
Kieran Lawrence <kieran.lawrence@hotmail.com>
2023-10-04 14:32:18 +0800

    Create helper functions to handle new date structure

--- frontend/src/utils/helpers.tsx
+++ frontend/src/utils/helpers.tsx
@@ -1,5 +1,8 @@
 import { format, differenceInHours } from 'date-fns';
 import formatRelative from 'date-fns/format';
+import { ArticleSection, Intention } from './types';
+import { ArticleSectionImageStyle, ArticleSectionStyle, QuoteContainerStyle } from './styles';
+import { Link } from 'react-router-dom';
 
 export const formatDate = (date: string) => {
 	var oneDayFromNow = new Date().getTime() + 1 * 24 * 60 * 60 * 1000;
@@ -12,4 +15,77 @@ export const formatDate = (date: string) => {
 };
 export const truncateArticleText = (desc: string, length: number) => {
 	return desc.length > length ? `${desc.substring(0, length)}...` : desc;
+};
+const formatIntentionKind = (intention: Intention, splitEnd: number, text: string) => {
+	switch (intention.kind) {
+		case 'important': {
+			return <strong>{text?.slice(intention.index, splitEnd)}</strong>;
+		}
+		case 'emphasized': {
+			return <em>{text?.slice(intention.index, splitEnd)}</em>;
+		}
+		case 'link': {
+			return <Link to={intention.link!}>{text?.slice(intention.index, splitEnd)}</Link>;
+		}
+		default: {
+			return <>{text?.slice(intention.index, splitEnd)}</>;
+		}
+	}
+};
+const formatIntention = (intentions: Intention[], text: string) => {
+	if (!text) return null;
+	const sorted = intentions.sort((a: Intention, b: Intention) => {
+		if (a.index < b.index) return -1;
+		else return 1;
+	});
+	const styledIntention = sorted.map((intention, index, elements) => {
+		let splitEnd = intention.index + intention.length;
+		var next = elements[index + 1];
+		return (
+			<>
+				{index === 0 && text!.slice(0, intention.index)}
+				{intention.kind === 'emphasized' ? <em>{text!.slice(intention.index, splitEnd)}</em> : <>{formatIntentionKind(intention, splitEnd, text)}</>}
+				{!next && text!.slice(splitEnd)}
+				{next && text!.slice(splitEnd, next.index)}
+			</>
+		);
+	});
+	return styledIntention;
+};
+export const formatArticleSections = (section: ArticleSection) => {
+	const { kind, intentions, text, attribution, url } = section;
+
+	switch (kind) {
+		case 'text': {
+			if (intentions && intentions.length > 0) {
+				return <ArticleSectionStyle>{formatIntention(intentions, text!)}</ArticleSectionStyle>;
+			} else return <ArticleSectionStyle>{text}</ArticleSectionStyle>;
+		}
+		case 'heading': {
+			return (
+				<ArticleSectionStyle>
+					<h2>{text}</h2>
+				</ArticleSectionStyle>
+			);
+		}
+		case 'quote': {
+			return (
+				<QuoteContainerStyle>
+					<section>{text}</section>
+					<small>{attribution}</small>
+				</QuoteContainerStyle>
+			);
+		}
+		case 'image': {
+			return (
+				<ArticleSectionImageStyle>
+					<img src={section.url} alt={section.text} />
+					{intentions && intentions.length > 0 ? <div>{formatIntention(intentions, text!)}</div> : <div>{section.text}</div>}
+				</ArticleSectionImageStyle>
+			);
+		}
+		default: {
+			return <ArticleSectionStyle>{text}</ArticleSectionStyle>;
+		}
+	}
 };
--- frontend/src/utils/styles.tsx
+++ frontend/src/utils/styles.tsx
@@ -34,7 +34,7 @@ export const FooterSocialLinksListStyle = styled.ul`
 	justify-content: space-between;
 	align-items: center;
 	list-style: none;
-	width: 60%;
+	width: 80%;
 	padding: 10px 0;
 	.socialIcons {
 		background-color: #e9353b;
@@ -120,4 +120,49 @@ const dynamicStyle = (props: SkeletonProps) =>
 
 export const SkeletonStyle = styled.div`
 	${dynamicStyle};
+`;
+export const ArticleSectionStyle = styled.div`
+	margin: 0 10px 20px 0;
+	font-size: 16px;
+	h2 {
+		font-size: 22px;
+		font-weight: 600;
+		padding-left: 5px;
+	}
+`;
+export const QuoteContainerStyle = styled.div`
+	border-left: 5px solid #e9353b;
+	padding-left: 15px;
+	margin: 24px 72px;
+	font-size: 20px;
+	font-style: italic;
+	font-weight: 400;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+
+	small {
+		margin-top: 8px;
+		font-size: 14px;
+		font-style: normal;
+	}
+`;
+export const ArticleSectionImageStyle = styled.div`
+	display: flex;
+	justify-content: left;
+	place-items: center;
+	flex-direction: column;
+	padding: 15px 0;
+
+	img {
+		max-width: 65%;
+		border-radius: 5px;
+		margin-right: auto;
+	}
+	div {
+		padding: 8px;
+		max-width: 65%;
+		font-size: 14px;
+		margin-right: auto;
+	}
 `;
